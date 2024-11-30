37798147d86067758615e18f3bfcb726c1562e5c
launchland <benjulian.noel@gmail.com>
2024-11-29 20:18:40 -0700

    Update scripts.js

--- JS and Deploy Site/scripts/scripts.js
+++ JS and Deploy Site/scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** SITE: https://launchland.github.io/robot-rentals/
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -10,6 +10,7 @@ You are encouraged to use the provided naming convention for ease of review.
 /* create variables to hold the values for modelName and duration */
 
 // INSERT YOUR CODE HERE
+window.addEventListener("DOMContentLoaded", () => {
 let modelName = "XYZ";
 let duration = 0;
 
@@ -80,6 +81,7 @@ modelButton.addEventListener("click", changeModel);
 
 // INSERT YOUR CODE HERE
 
+
 let durationButton = document.getElementById("duration-button");
 
 function changeDuration() {
@@ -95,3 +97,4 @@ function changeDuration() {
 }
 
 durationButton.addEventListener("click", changeDuration);
+});
